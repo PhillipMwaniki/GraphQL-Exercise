c9d5479a0700294b429f9735779423f2ec96f9db
Phillip Nzuli <mwanikiphillip@gmail.com>
2017-08-01 22:26:38 +0300

    Now using axios to return data

--- schema.js
+++ schema.js
@@ -41,13 +41,15 @@ const RootQuery = new GraphQLObjectType({
                 //     }
                 // }
 
-                
+                return axios.get('http://localhost:3000/customers/' + args.id)   
+                    .then(res => res.data);
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args) {
-                return customers;
+                return axios.get('http://localhost:3000/customers/')
+                    .then(res => res.data);
             }
         }
     }
